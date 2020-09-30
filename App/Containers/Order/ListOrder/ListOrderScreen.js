52763b2b3c0e9afd0311890bf1f4f75a1d6d96ce
vsmevn <chplay.cntt.biz@gmail.com>
2020-09-30 15:11:11 +0700

    add color for tab order

--- App/Containers/Order/ListOrder/ListOrderScreen.js
+++ App/Containers/Order/ListOrder/ListOrderScreen.js
@@ -6,7 +6,8 @@ import {
 } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
-import {commom, Colors} from "~/Themes";
+import { ORDER_TYPE } from '~/Constants/Config';
+import { commom, Colors } from "~/Themes";
 import InputDefault from '~/Components/Inputs';
 import styles from './styles'
 import OrderTab from '../OrderTab/OrderTab';
@@ -24,8 +25,8 @@ const renderTabBar = props => (
         <Text style={[styles.labelStl, {color: focused ? Colors.blue: Colors.black}  ]}>
           {route.title}
         </Text>
-        <View style={styles.badge}>
-          <Text style={styles.badgeNumber}>2</Text>
+        <View style={[styles.badge, {backgroundColor: route.color}]}>
+          <Text style={styles.badgeNumber}>2</Text>          
         </View>
       </View>
       
@@ -34,43 +35,50 @@ const renderTabBar = props => (
 )
 
 const renderScene = SceneMap({
-  all: () => <OrderTab/>,
-  pending: () => <OrderTab/>,
-  reported: () => <OrderTab/>,
-  khoTQ: () => <OrderTab/>,
-  khoVN: () => <OrderTab/>,
-  doiSoat: () => <OrderTab/>,
-  dangGoi: () => <OrderTab/>,
-  dagiao: () => <OrderTab/>,
-  danhan: () => <OrderTab/>,
-  hoanthanh: () => <OrderTab/>,
-  hangloi: () => <OrderTab/>,
-  huydon: () => <OrderTab/>
+  all: () => <OrderTab type={ORDER_TYPE.all}/>,
+  pending: () => <OrderTab type={ORDER_TYPE.pending}/>,
+  reported: () => <OrderTab type={ORDER_TYPE.reported}/>,
+  khoTQ: () => <OrderTab type={ORDER_TYPE.khoTQ}/>,
+  khoVN: () => <OrderTab type={ORDER_TYPE.khoVN}/>,
+  doiSoat: () => <OrderTab type={ORDER_TYPE.doiSoat}/>,
+  dangGoi: () => <OrderTab type={ORDER_TYPE.dangGoi}/>,
+  dagiao: () => <OrderTab type={ORDER_TYPE.dagiao}/>,
+  danhan: () => <OrderTab type={ORDER_TYPE.danhan}/>,
+  hoanthanh: () => <OrderTab type={ORDER_TYPE.hoanthanh}/>,
+  hangloi: () => <OrderTab type={ORDER_TYPE.hangloi}/>,
+  huydon: () => <OrderTab type={ORDER_TYPE.huydon}/>
 })
 
 export default class ListOrderScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      querySearch: '',
       index: 0,
       routes: [
-        { key: 'all', title: 'Tất cả' },
-        { key: 'pending', title: 'Chờ duyệt' },
-        { key: 'reported', title: 'Đã báo' },
-        { key: 'khoTQ', title: 'Đã về kho TQ' },
-        { key: 'khoVN', title: 'Đã về kho VN' },
-        { key: 'doiSoat', title: 'Đối soát' },
-        { key: 'dangGoi', title: 'Đang gói hàng' },
-        { key: 'dagiao', title: 'Đã giao hàng' },
-        { key: 'danhan', title: 'Khách đã nhận' },
-        { key: 'hoanthanh', title: 'Hoàn thành' },
-        { key: 'hangloi', title: 'Hàng lỗi' },
-        { key: 'huydon', title: 'Hủy đơn' }
+        { key: 'all', title: 'Tất cả', color: 'red' },
+        { key: 'pending', title: 'Chờ duyệt', color: Colors.pending },
+        { key: 'reported', title: 'Đã báo', color: Colors.pending },
+        { key: 'khoTQ', title: 'Đã về kho TQ', color: Colors.pending },
+        { key: 'khoVN', title: 'Đã về kho VN', color: Colors.pending },
+        { key: 'doiSoat', title: 'Đối soát', color: Colors.pending },
+        { key: 'dangGoi', title: 'Đang gói hàng', color: Colors.pending },
+        { key: 'dagiao', title: 'Đã giao hàng', color: Colors.pending },
+        { key: 'danhan', title: 'Khách đã nhận', color: Colors.pending },
+        { key: 'hoanthanh', title: 'Hoàn thành', color: Colors.pending },
+        { key: 'hangloi', title: 'Hàng lỗi', color: Colors.pending },
+        { key: 'huydon', title: 'Hủy đơn', color: Colors.pending }
       ]
     }
   }
+
+  search() {
+    const { query } = this.state;
+    alert(query);
+  }
+
   render() {
-    const { index, routes } = this.state;
+    const { index, routes, querySearch } = this.state;
     const initialLayout = { 
       width: Dimensions.get('window').width 
     }
@@ -80,8 +88,11 @@ export default class ListOrderScreen extends Component {
          <InputDefault
           placeholder='Tìm kiếm đơn hàng' 
           iconRight
+          value={querySearch}
+          onChangeText={text => this.setState({querySearch: text})}
           inputWrapStl={styles.inputSearch}
           containerStl={commom.mx15}
+          submit={() => this.search()}
         />
         <TabView      
           renderTabBar={renderTabBar}
